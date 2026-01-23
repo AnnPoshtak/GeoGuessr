b9556e949b8d919625bb798000b070cd86512440
sad1dab <a.poshtak2011@gmail.com>
2026-01-23 20:49:06 +0200

    feat(pages): add routing and SinglePlayer page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,11 @@
-import StreetView from './components/StreetView/StreetView.tsx';
+import {Route, Routes} from "react-router-dom";
+import SinglePlayer from "./pages/SinglePlayer/SinglePlayer.tsx";
 
-function App() {
-    const apiKey = "API_KEY";
-
+function App(){
     return (
-        <div className="App">
-            <h1>StreetView Map</h1>
-            <StreetView apiKey={apiKey} zoom={14} center={{lat: 48.8625, lng: 2.2882}} style={{width: "100%", height: '100vh', position: "absolute", top: 0, right: 0}} options={{pov: { heading: 135, pitch: 5 }, zoom: 0.5, motionTracking: false, addressControl: false, fullscreenControl: false}}/>
-        </div>
-    );
+        <Routes>
+            <Route path="/" element={<SinglePlayer/>}></Route>
+        </Routes>
+    )
 }
-
 export default App;
---
+++ frontend/src/pages/SinglePlayer/SinglePlayer.tsx
@@ -0,0 +1,14 @@
+import StreetView from "../../components/StreetView/StreetView.tsx";
+
+function SinglePlayer() {
+    const apiKey = import.meta.env.VITE_GOOGLE_API;
+
+    return (
+        <div className="App">
+            <h1>StreetView Map</h1>
+            <StreetView apiKey={apiKey} zoom={14} center={{lat: 48.8625, lng: 2.2882}} style={{width: "100%", height: '100vh', position: "absolute", top: 0, right: 0}} options={{pov: { heading: 135, pitch: 5 }, zoom: 0.5, motionTracking: false, addressControl: false, fullscreenControl: false}}/>
+        </div>
+    );
+}
+
+export default SinglePlayer;
