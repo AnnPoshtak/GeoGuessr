d027a7723bca9c6d6cea8c857bdd3cfb65c9c80e
Fundi1330 <lerchmap@gmail.com>
2026-05-26 22:30:52 +0300

    chore: add FLASK_DEBUG to .env.example

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -16,5 +16,5 @@ if __name__ == '__main__':
         app,
         host=os.environ.get('HOST', '0.0.0.0'),
         port=int(os.environ.get('PORT', '5000')),
-        debug=os.environ.get('FLASK_DEBUG'),
+        debug=bool(os.environ.get('FLASK_DEBUG')),
     )
