d46c5b9412f54c9cb131e0328776442e9ee0469a
Fundi1330 <lerchmap@gmail.com>
2026-06-03 09:25:57 +0300

    feat: notify when player dis/reconnects to the game

--- backend/app/ws/ws.py
+++ backend/app/ws/ws.py
@@ -1,4 +1,5 @@
 from app import socketio
+from flask import current_app
 from .game import GameNamespace
 from .queue import QueueNamespace
 
@@ -6,5 +7,5 @@ socketio.on_namespace(QueueNamespace('/queue'))
 socketio.on_namespace(GameNamespace('/game'))
 
 @socketio.on_error_default
-def handle_error(e):
-    print(f'A SocketIO error occured: {e}')
+def handle_error(e: Exception):
+    current_app.logger.error(f'A SocketIO error occured: {e}', exc_info=True)
